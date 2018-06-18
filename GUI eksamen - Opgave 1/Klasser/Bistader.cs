e6f73e4d75f442cd696a1dd4b35b31ad22ee0d8d
Tobias Saaby Steffensen <tobias.saaby@live.com>
2018-06-18 10:52:32 +0200

    Nye vinduer

---
+++ GUI eksamen - Opgave 1/Klasser/Bistader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GUI_eksamen___Opgave_1
+{
+    class Bistader : ObservableCollection<MideTal>, INotifyPropertyChanged
+    {
+        private string name;
+        private List<MideTal> stadeTal;
+
+        public Bistader(string navn)
+        {
+            name = navn;
+            stadeTal = new List<MideTal>();
+        }
+
+        public string Name { get { return name; } set { name = value; NotifyPropertyChanged(); } }
+        public List<MideTal> StadeTal { get { return stadeTal; } set { stadeTal = value; NotifyPropertyChanged(); } }
+
+
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ GUI eksamen - Opgave 1/Klasser/MideTal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_eksamen___Opgave_1
+{
+    class MideTal : INotifyPropertyChanged
+    {
+        private DateTime dato;
+        private int antal;
+        private string kommentar;
+
+        public MideTal(DateTime Dato, int Antal, string Kommentar)
+        {
+            dato = Dato;
+            antal = Antal;
+            kommentar = Kommentar;
+        }
+
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
